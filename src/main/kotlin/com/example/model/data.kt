aacfb6d8da0c11f23b9aaa9cadc1604828773ce9
ahmed <elnomeryahmed56@gmail.com>
2023-12-10 11:28:24 +0200

    a

---
+++ src/main/kotlin/com/example/model/data.kt
@@ -0,0 +1,10 @@
+package com.example.model
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class data(
+    val key:String,
+    val value:String
+)
---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,22 @@
+@file:Suppress("DEPRECATION")
+
+package com.example.plugins
+
+import com.example.route.getdata
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        getdata()
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        // Static plugin. Try to access `/static/index.html`
+        static("/static") {
+            resources("static")
+        }
+    }
+}
